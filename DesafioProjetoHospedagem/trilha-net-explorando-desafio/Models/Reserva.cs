c6b6d52ad5eec6354102a13237cee34df8a0582b
RenathaMachado <renatharenascer@icloud.com>
2022-10-19 22:52:38 -0300

    Conclusão do Projeto

---
+++ DesafioProjetoHospedagem/trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("O número de hóspedes é superior à capacidade da Suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            int quantidade = Hospedes.Count;
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = Convert.ToDecimal(0.1)*valor;
+                valor-= desconto;
+            }
+
+            return valor;
+        }
+    }
+}
